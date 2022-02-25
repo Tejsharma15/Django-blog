124ec8c846039d954dff6e1ad4c493f3d4343065
Tejsharma15 <77492919+Tejsharma15@users.noreply.github.com>
2022-02-25 12:21:35 +0530

    User files added

--- models.py
+++ models.py
@@ -1,18 +1,20 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
+from PIL import Image
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length = 50)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default = timezone.now)
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return self.title
+        return f'{self.user.username} Profile'
     
-    def get_absolute_url(self):
-        return reverse('Post-Detail', kwargs={'pk':self.pk})
+    def save(self):
+        super().save()
 
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
